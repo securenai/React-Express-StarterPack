46a02fb35359e903113fa61f0750501bd2c8a650
乃軒 securenai <michaelnccu@gmail.com>
2020-10-04 14:50:42 +0800

    change server listen port to 5000

--- server.js
+++ server.js
@@ -2,10 +2,10 @@ const express = require('express');
 
 const app = express();
 
-const PORT = '3000';
+const PORT = '5000';
 
 app.use(express.json());
 
 app.listen(PORT, () => {
-	console.log('App listening on port 3000!!!');
+	console.log('App listening on port 5000!!!');
 });
